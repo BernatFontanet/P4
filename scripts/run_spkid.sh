f191f97ac35b0ab2a8570308098e72aebb3b9183
crisgc810 <114924341+crisgc810@users.noreply.github.com>
2022-12-28 21:24:53 +0100

    test

--- scripts/run_spkid.sh
+++ scripts/run_spkid.sh
@@ -18,10 +18,9 @@ set -o pipefail
 # - db_devel: directory of the speecon database used during development
 # - db_test:  directory of the database used in the final test
 lists=lists
-w=work #si hacemos lp -> work/lp, si hacemos mfcc ->work/mfcc, si hacemos gmm de lp-> work/gmm/lp
-#work/ficheros_temporales_y_de_resultado
-name_exp=one #cambiar nombre a los ficheros temp y de resultados
-db_devel=spk_8mu/speecon #
+w=work 
+name_exp=one 
+db_devel=spk_8mu/speecon 
 db_test=spk_8mu/sr_test
 world=users
 # Ficheros de resultados del reconocimiento y verificación
@@ -82,13 +81,12 @@ compute_lp() {
     shift
     for filename in $(sort $*); do
         mkdir -p `dirname $w/$FEAT/$filename.$FEAT`
-        EXEC="wav2lp 26 $db/$filename.wav $w/$FEAT/$filename.$FEAT"
+        EXEC="wav2lp 20 $db/$filename.wav $w/$FEAT/$filename.$FEAT"
         echo $EXEC && $EXEC || exit 1
     done
 }
 
 compute_lpcc() {
-#Con el comando shift el $1 desaparece y los otros decrementan una unidad, entonces $* no incluye el primero pero si todos
     db=$1
     shift
     for filename in $(sort $*); do
@@ -99,7 +97,6 @@ compute_lpcc() {
 }
 
 compute_mfcc() {
-#Con el comando shift el $1 desaparece y los otros decrementan una unidad, entonces $* no incluye el primero pero si todos
     db=$1
     shift
     for filename in $(sort $*); do
@@ -137,7 +134,7 @@ for cmd in $*; do
        for dir in $db_devel/BLOCK*/SES* ; do
            name=${dir/*\/}
            echo $name ----
-           EXEC="gmm_train -v 1 -T 0.001 -N 100 -m 32 -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$name.gmm $lists/class/$name.train -i 1"
+           EXEC="gmm_train -v 1 -T 0.0001 -N 100 -m 32 -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$name.gmm $lists/class/$name.train -i 1"
            echo $EXEC && $EXEC || exit 1
            echo
        done
@@ -163,7 +160,7 @@ for cmd in $*; do
        # Implement 'trainworld' in order to get a Universal Background Model for speaker verification
        #
        # - The name of the world model will be used by gmm_verify in the 'verify' command below.
-        EXEC="gmm_train -v 1 -T 0.001 -N 100 -m 32 -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$world.gmm $lists/verif/$world.train -i 1"
+        EXEC="gmm_train -v 1 -T 0.0001 -N 100 -m 32 -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$world.gmm $lists/verif/$world.train -i 1"
         echo $EXEC && $EXEC || exit 1
      
 
@@ -226,7 +223,7 @@ for cmd in $*; do
        EXEC="gmm_verify -d $w/$FEAT -e $FEAT -D $w/gmm/$FEAT -E gmm -w $world $lists/gmm.list $lists/final/verif.test $lists/final/verif.test.candidates"
        echo $EXEC && $EXEC | tee $TEMP_VERIF || exit 1
         perl -ane 'print "$F[0]\t$F[1]\t";
-            if ($F[2] > 0.2593305431556) {print "1\n"}
+            if ($F[2] > 0.251449340984402) {print "1\n"}
             else {print "0\n"}' $TEMP_VERIF | tee $FINAL_VERIF
    # If the command is not recognize, check if it is the name
    # of a feature and a compute_$FEAT function exists.
